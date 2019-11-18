08c741d00259bdfd5bea5b8d54962ab3a4035ea0
Austin <austin.dober@gmail.com>
2019-11-17 23:13:27 -0500

    fixing href issues

--- client/src/components/MainContainer/index.js
+++ client/src/components/MainContainer/index.js
@@ -16,17 +16,17 @@ function MainContainer(){
     return(
         <div>
             <nav className="navbar navbar-expand-lg " style={styling.header}>
-                <a className="navbar-brand" href="#">Google Book Search</a>
+                <a className="navbar-brand" href="/">Google Book Search</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Home</a>
+                            <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Saved Books</a>
+                            <a className="nav-link" href="/saved">Saved Books</a>
                         </li>
                     </ul>
                 </div>
