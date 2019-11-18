5b6c99982e4ed21dfa3f500f5d8e4ff2c864b11a
Austin <austin.dober@gmail.com>
2019-11-17 22:05:39 -0500

    adding working API

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 const styling={
     searchbar: {
         margin: "20px auto",
@@ -9,32 +11,58 @@ const styling={
 
 
 function BookSearch(){
+
+    const [book, setBook] = useState('');
+    const [results, setResults] = useState([]);
+    const [APIKey, setAPIKey] = useState('AIzaSyDz2jlBCa3uarUkN6NL7pia07KhtHKGEBc');
+
+    function handleChange(event){
+
+        const book = event.target.value;
+
+        setBook(book);
+
+    };
+    function handleSubmit(event){
+
+        event.preventDefault();
+
+        axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+APIKey+"&maxResults=40")
+        .then(data =>{
+            setResults(data.data.items);
+        })
+
+    };
+
     return(
         <div className="container">
-            <div>
-                <input 
-                style={styling.searchbar}
-                type='text'
-                name='search'
-                placeholder="Book Name"></input>
-            </div>
-            <div>
-                <h4> Or </h4>
-            </div>
-            <div>
-                <input 
-                style={styling.searchbar}
-                type='text'
-                name='search'
-                placeholder="Author Name"></input>
-            </div>
-            <div>
-                <button 
-                type="submit"
-                className="btn btn-primary"
-                >
-                Search</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input 
+                    style={styling.searchbar}
+                    type='text'
+                    name='search'
+                    placeholder="Book Name"
+                    onChange={handleChange}></input>
+                </div>
+                <div>
+                    <h4> Or </h4>
+                </div>
+                <div>
+                    <input 
+                    style={styling.searchbar}
+                    type='text'
+                    name='search'
+                    placeholder="Author Name"></input>
+                </div>
+                <div>
+                    <button 
+                    type="submit"
+                    className="btn btn-primary"
+                    >
+                    Search</button>
+                </div>
+            </form>
         </div>      
     )
 }
