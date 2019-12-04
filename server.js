4f52a753d880be13e586c68f4160dacc08b2ddd3
Austin Dober <48484800+austdobe@users.noreply.github.com>
2019-12-04 16:11:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds229088.mlab.com:29088/heroku_tp7zfp5m";
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds251948.mlab.com:51948/heroku_z7g1h104"
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
 );
